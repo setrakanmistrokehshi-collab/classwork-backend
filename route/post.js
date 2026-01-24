d9a2e7b76b8eb37e69aac0bae699225563487c75
setrakanmistrokehshi <setrakanmistrokehshi@gmail.com>
2026-01-24 15:57:40 +0100

    classwork-backend

--- route/post.js
+++ route/post.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const authMiddleware = require('../Middleware/authMiddleware.js');  // Adjust path
+const authMiddleware = require('../classwork-backend/Middleware/authMiddleware.js');  // Adjust path
 const Post = require('../../model/post');
 
 const router = express.Router();
